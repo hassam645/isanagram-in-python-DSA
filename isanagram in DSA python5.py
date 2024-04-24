364e4b41bae7c1754dad07050bce8aea94e6f745
hassam bin shahid <139114049+hassam645@users.noreply.github.com>
2024-04-24 02:19:15 -0700

    Add files via upload

---
+++ isanagram in DSA python5.py
@@ -0,0 +1,11 @@
+class solution:
+    def isanagram(self,s:str,t:str)->bool:
+        if len(s)!=len(t):
+            return False
+        char_count_s=[0]*26
+        char_count_t=[0]*26
+        for char in s:
+            char_count_s[ord(char)-ord('a')]+=1
+            for char in t:
+                char_count_t[ord(char)-ord('a')]-=1
+                return char_count_s==char_count_t
---
+++ isanagram in DSA python6.py
@@ -0,0 +1,4 @@
+class solution:
+    def isanagram(self,s:str,t:str)->bool:
+        return(sorted(s.lower))==(sorted(t.lower))
+    return(counter(s.lower))==(counter(t.lower))
